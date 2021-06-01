fe397309ed9a233b914cad4a0d6fa3f14e990f5e
xbc <327340773@qq.com>
2021-06-01 17:50:19 +0800

    [main] css

--- content.go
+++ content.go
@@ -15,7 +15,7 @@ type Content struct {
 func NewContent() *Content {
 	c := &Content{}
 	c.body = NewNode("body")
-	c.Css = "table\n{border-collapse:collapse;\n table-layout:fixed;\n}\n table, td, th {border:1px solid black;padding: 5px;padding-left: 10px;    padding-right: 10px;}\n" +
+	c.Css = "body { font-family:  \"Consolas\" ; } \n table\n{border-collapse:collapse;\n table-layout:fixed;\n}\n table, td, th {border:1px solid black;padding: 5px;padding-left: 10px;    padding-right: 10px;}\n" +
 		"pre { padding: 4pt; max-width: 100%%white-space; line-height: 1.5; border: 1pt solid #ddd; background-color: #f7f7f7;  }\n" +
 		"code { font-family: DejaVu Sans Mono, \\\\5FAE\\\\8F6F\\\\96C5\\\\9ED1; line-height: 1.5; background-color: #f7f7f7; }\n"
 	return c
